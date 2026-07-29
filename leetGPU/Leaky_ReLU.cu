f2e1aa6f911fa6d1f18d09a17380baee15ffa06e
Saikat-too <saikatsingha00q@gmail.com>
2026-07-29 19:58:13 +0600

    Leaky_ReLU in CUDA

---
+++ leetGPU/Leaky_ReLU.cu
@@ -0,0 +1,19 @@
+#include <cuda_runtime.h>
+
+__global__ void leaky_relu_kernel(const float* input, float* output, int N) {
+    int idx = threadIdx.x + blockDim.x * blockIdx.x;
+    float alpha = 0.01;
+
+    if (idx < N) {
+        output[idx] = (input[idx] < 0) ? alpha * input[idx] : input[idx];
+    }
+}
+
+// input, output are device pointers (i.e. pointers to memory on the GPU)
+extern "C" void solve(const float* input, float* output, int N) {
+    int threadsPerBlock = 256;
+    int blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock;
+
+    leaky_relu_kernel<<<blocksPerGrid, threadsPerBlock>>>(input, output, N);
+    cudaDeviceSynchronize();
+}
